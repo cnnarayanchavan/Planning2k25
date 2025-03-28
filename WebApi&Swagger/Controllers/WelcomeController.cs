ec52ecc0323ef73ad23e525c60a26bf34ba27cdf
cnnarayanchavan <cnnarayanchavan@gmail.com>
2025-03-28 22:58:45 +0530

    Endpoints and Swagger integration

---
+++ WebApi&Swagger/Controllers/WelcomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi_Swagger.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WelcomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult CallUser()
+        {
+            return Ok("Hey Theer!! Web Api Core this side");
+        }
+
+        [HttpPost]
+        public IActionResult CreateUser(string Name)
+        {
+            return Ok("Post method to add data");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateUser()
+        {
+            return Ok("Put method to update data");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteUser() 
+        {
+            return Ok("Delete method to remove user data");
+        }
+    }
+}
